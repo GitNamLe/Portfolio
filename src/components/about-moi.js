eb9b88e3266d234ae09af80734a2bee13ff0f0f5
NamLe <vnamle1@gmail.com>
2018-01-10 23:55:37 -0500

    Editing About Me

--- src/components/about-moi.js
+++ src/components/about-moi.js
@@ -6,7 +6,7 @@ export default function AboutMoi(){
 
             <div className="row">
                 <div className="col-xs-12">
-                    <h2>I Knowsss...</h2>
+                    <h2>My Experience</h2>
                 </div>
             </div>
 
@@ -16,8 +16,8 @@ export default function AboutMoi(){
                     <p>I have worked with clients to get what they really need in their website. Its my job to convey what they mean into something beatiful.</p>
                 </div>
                 <div className="col-sm-4">
-                    <h4>UI/UX Experience</h4>
-                    <p>I have worked with clients to get what they really need in their website. Its my job to convey what they mean into something beatiful.</p>
+                    <h4>HTML/JS/CSS</h4>
+                    <p>Coding to create meanigful websites with simple tools.</p>
                 </div>
                 <div className="col-sm-4">
                     <h4>UI/UX Experience</h4>
